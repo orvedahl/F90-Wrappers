24dc198b7c2a87efbc4a2d32644caef682467cc4
Ryan Orvedahl <ryan.orvedahl@gmail.com>
2015-02-22 15:39:00 -0700

    make source & MESA & python with SAME compiler version, remove unused(?) klu library

--- Cython/MESA-Test/setup.py
+++ Cython/MESA-Test/setup.py
@@ -13,6 +13,7 @@ from distutils.extension import Extension
 from Cython.Distutils import build_ext
 import numpy
 import os
+import sys
 
 mesa_dir = os.environ['MESA_DIR']  # uses MESA_DIR environment variable
                                    # that is required to install MESA
@@ -29,16 +30,21 @@ pyx_name = "cython_wrapper"
 #libraries = ['net', 'eos', 'rates', 'chem', 'interp_2d',
 libraries = ['gfortran', 'net', 'eos', 'rates', 'chem', 'interp_2d',
              'interp_1d', 'num', 'crlibm', 'mtx', 'const', 'utils',
-             'mesaklu', 'mesalapack', 'mesablas']
+             'mesalapack', 'mesablas']
+             #'mesaklu', 'mesalapack', 'mesablas']
 lib_dirs = [mesa_lib]
 include_dirs = [npy_include_dir, mesa_inc, python_inc]
 
 # list the object files: wrapper.o holds the wrapping code which depends on
 # original.o code...note this is the filename, not the module name
-objects = ["data_types.o", "errors.o", "mesa_utils.o", "eos/eos_utils.o",
-        "eos/setup_mesa_eos.o", "eos/shutdown_mesa_eos.o",
-        "network/net_utils.o", "eos/MESA_EOS.o", "network/setup_mesa_net.o",
-        "network/shutdown_mesa_net.o", "network/call_mesa_net.o", "wrapper.o"]
+objects = ["data_types.o", "errors.o", "mesa_utils.o", "eos_utils.o",
+        "setup_mesa_eos.o", "shutdown_mesa_eos.o",
+        "net_utils.o", "MESA_EOS.o", "setup_mesa_net.o",
+        "shutdown_mesa_net.o", "call_mesa_net.o", "wrapper.o"]
+#objects = ["data_types.o", "errors.o", "mesa_utils.o", "eos/eos_utils.o",
+#        "eos/setup_mesa_eos.o", "eos/shutdown_mesa_eos.o",
+#        "network/net_utils.o", "eos/MESA_EOS.o", "network/setup_mesa_net.o",
+#        "network/shutdown_mesa_net.o", "network/call_mesa_net.o", "wrapper.o"]
 
 ext_modules = [Extension(pyx_name, [pyx_file],
                          include_dirs=include_dirs,
